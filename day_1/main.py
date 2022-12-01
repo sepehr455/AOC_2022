64775bd328e2396b29a52c01359184da6b91843f
sepehr455 <84247783+sepehr455@users.noreply.github.com>
2022-12-01 15:17:55 -0800

    s

---
+++ day_1/main.py
@@ -0,0 +1,26 @@
+import math
+
+
+def maxNum(limit):
+    f = open("input.txt")
+    max_cals = 0
+
+    temp_max_cals = 0
+
+    for i in f.readlines():
+        if i.strip() == "":
+            if max_cals < temp_max_cals < limit:
+                max_cals = temp_max_cals
+            temp_max_cals = 0
+
+        else:
+            temp_max_cals += int(i)
+    return max_cals
+
+
+first_num = maxNum(math.inf)
+second_num = maxNum(first_num)
+third_num = maxNum(second_num)
+result = first_num + second_num + third_num
+
+print(result)
